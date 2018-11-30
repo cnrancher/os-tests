18b96b3fcf2cc6b8e63871de0b704e7cf79fc41e
Noctis Lucis <urakiny@gmail.com>
2018-11-30 14:53:47 +0800

    Modify the connection function

--- utils/connect_to_os.py
+++ utils/connect_to_os.py
@@ -8,26 +8,33 @@ import time
 
 import paramiko
 
+SSH_KEY_PATH = '/opt/os-tests/assets/id_rsa'
 
-def connection(ip):
-    for _ in range(30):
-        time.sleep(10)
+
+def connection(ip, seconds):
+    for _ in range(40):
+        if seconds:
+            time.sleep(seconds)
+        else:
+            time.sleep(10)
         try:
             ssh = paramiko.SSHClient()
+            private_key = paramiko.RSAKey.from_private_key_file(SSH_KEY_PATH)
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
             ssh.connect(hostname=ip,
                         username='rancher',
-                        password='')
+                        password='',
+                        pkey=private_key)
+            # The issue can solve 'SSH session not active'  https://github.com/paramiko/paramiko/issues/928
             if ssh.get_transport().active:
                 break
         except:
             ssh.close()
-
     return ssh
 
 
-def executor(client, command, seconds=40):
+def executor(client, command, seconds=100):
     assert (client and command is not None)
     stdin, stdout, stderr = client.exec_command(command, timeout=seconds)
     output = stdout.read().decode('utf-8')
