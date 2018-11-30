142b1f7b1ca3b9d2f3bc4217d72d8a5ccc6453d5
Noctis Lucis <urakiny@gmail.com>
2018-11-30 14:52:29 +0800

    Remove cloud config parameters

--- executor.py
+++ executor.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
     try:
         pytest.main(
             ["-s",
-             BASE_DIR + '{script_package}'.format(script_package='/project_path/engine/'),
-             '--cloud_config_url={url}'.format(url='http://192.168.1.24/ros')])
+             BASE_DIR + '{script_package}'.format(script_package='/code/engine/test_preload.py')])
     except Exception as e:
         print(e.args[0])
