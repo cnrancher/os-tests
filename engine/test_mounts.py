974b985e9a17b82d774d9e1a64178181be9c45ec
Noctis Lucis <urakiny@gmail.com>
2018-12-02 18:01:35 +0800

    Increase the timeout time.

--- engine/test_mounts.py
+++ engine/test_mounts.py
@@ -11,15 +11,15 @@ def test_mounts(ros_kvm_init, cloud_config_url):
                   is_second_hd=True)
     tuple_return = ros_kvm_init(**kwargs)
     client = tuple_return[0]
-    stdin, stdout, stderr = client.exec_command(command, timeout=60)
+    stdin, stdout, stderr = client.exec_command(command, timeout=100)
     output = stdout.read().decode('utf-8')
     assert ('test' in output)
 
-    command_mk = 'sudo mkfs.ext4 /dev/vdb && sudo cloud-init-execute'
-    client.exec_command(command_mk, timeout=60)
+    command_mk = 'sudo mkfs.ext4 /dev/vdb ; sudo cloud-init-execute'
+    client.exec_command(command_mk, timeout=100)
     # Network delay
-    time.sleep(5)
-    stdin, stdout, stderr = client.exec_command('mount', timeout=60)
+    time.sleep(20)
+    stdin, stdout, stderr = client.exec_command('mount', timeout=100)
     output = stdout.read().decode('utf-8')
     client.close()
     assert ('/home/rancher/a' in output
